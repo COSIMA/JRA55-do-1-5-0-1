51ddcdfe0a05569ed81349c0815a84760ed92103
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2023-08-07 10:59:12 +1000

    allow parallel updating

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # JRA55-do v1.5.0.1
 
-JRA55-do v1.5.0.1 is an atmospheric dataset designed for forcing ocean and sea ice models, downloaded from https://climate.mri-jma.go.jp/pub/ocean/JRA55-do/
+JRA55-do v1.5.0.1 is an atmospheric dataset designed for forcing ocean and sea ice models, available from https://climate.mri-jma.go.jp/pub/ocean/JRA55-do/
 
 NCI already hosts JRA55-do v1.5.0 at `/g/data/qv56/replicas/input4MIPs/CMIP6/OMIP/MRI/MRI-JRA55-do-1-5-0` but that dataset finishes at 15 Jul 2020.
 JRA55-do v1.5.0.1 extends this to the present as a near-real-time dataset, updated daily, running from 1 Jan 2020 to 4 days behind the present day.
-Data for the past 30 days are preliminary, and replaced day-by-day by final, higher-quality data.
+Data for the past 30 days are preliminary, and replaced day-by-day by final, higher-quality data. See `check_JRA55do1p5p0p1.ipynb`.
 
-To update, do
+To download/update JRA55-do v1.5.0.1 data in directory "./mirror", do
 ```
 qsub update.sh
 ```
+You can do this several times if you want to download in parallel to speed it up.
+
+Partially-downloaded `.nc` files have `-partial` appended. Downloading is skipped for any `.nc` file for which a corresponding `.nc-partial` exists (so that downloads can proceed in parallel). Check for `-partial` files with `find . -name "*.nc-partial"` after all update jobs have completed. If any exist, you'll need to do
+```
+./update.py -d
+```
+to delete the partial files, then
+```
+qsub update.sh
+```
+to try downloading them again.
 
 Andrew Kiss (andrew.kiss@anu.edu.au)
-5 April 2022
--- update.py
+++ update.py
@@ -4,30 +4,49 @@ from urllib.parse import urljoin
 import os
 from bs4 import BeautifulSoup
 
-sourceurl = 'https://climate.mri-jma.go.jp/pub/ocean/JRA55-do/jra55do_latest.html'
+def update(delete_partial=False,
+           sourceurl='https://climate.mri-jma.go.jp/pub/ocean/JRA55-do/jra55do_latest.html',
+           basedir='mirror'):
 
-basedir = 'mirror'
-os.makedirs(basedir, exist_ok=True)
+    os.makedirs(basedir, exist_ok=True)
 
-urlretrieve(urljoin(sourceurl, 'index.html'), os.path.join(basedir, 'index.html'))
+    urlretrieve(urljoin(sourceurl, 'index.html'), os.path.join(basedir, 'index.html'))
 
-sourcefn = os.path.join(basedir, os.path.basename(sourceurl))
-urlretrieve(sourceurl, sourcefn)
+    sourcefn = os.path.join(basedir, os.path.basename(sourceurl))
+    urlretrieve(sourceurl, sourcefn)
 
-# download all .nc files linked from sourceurl, into the same directory structure
-soup = BeautifulSoup(open(sourcefn), 'html.parser')
-for link in soup.find_all('a'):
-    href = link.get('href')
-    if href.endswith('.nc'):
-        outfn = os.path.normpath(os.path.join(basedir, href))
-        outfntmp = outfn+'-partial'
-        if os.path.exists(outfn):
-            print('--- skipping', outfn, '(already exists)')
-        else:
-            print('downloading', outfn)
-            os.makedirs(os.path.dirname(outfn), exist_ok=True)
-            if os.path.exists(outfntmp):
-                os.remove(outfntmp)
-            urlretrieve(urljoin(sourceurl, href), outfntmp)
-            os.rename(outfntmp, outfn)
-print('done')
+    # download all .nc files linked from sourceurl, into the same directory structure
+    soup = BeautifulSoup(open(sourcefn), 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.endswith('.nc'):
+            outfn = os.path.normpath(os.path.join(basedir, href))
+            outfntmp = outfn+'-partial'
+            if os.path.exists(outfn):
+                print('--- skipping', outfn, '(already exists)')
+            elif os.path.exists(outfntmp):
+                if delete_partial:
+                    print('### DELETING', outfn)
+                    os.remove(outfntmp)
+                else:
+                    print('*** skipping', outfn, '(already downloading)')
+            else:
+                if not delete_partial:
+                    os.makedirs(os.path.dirname(outfn), exist_ok=True)
+                    print('downloading', outfn)
+                    urlretrieve(urljoin(sourceurl, href), outfntmp)
+                    os.rename(outfntmp, outfn)
+    print('done')
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description=
+        'Download/update JRA55-do v1.5.0.1 data in directory "./mirror".\
+        JRA55-do v1.5.0.1 is an atmospheric dataset designed for forcing ocean and sea ice models, available from https://climate.mri-jma.go.jp/pub/ocean/JRA55-do/.\
+        Latest version of this script: https://github.com/COSIMA/JRA55-do-1-5-0-1')
+    parser.add_argument('-d', '--delete_partial',
+                        action='store_true', default=False,
+                        help="delete partially-downloaded files; don't download anything")
+    args = parser.parse_args()
+    delete_partial = vars(args)['delete_partial']
+    update(delete_partial=delete_partial)
